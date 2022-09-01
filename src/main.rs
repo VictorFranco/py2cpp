b60aae83379c901ad63f34493e2a40f7b398b323
Victor Franco <vfrancohdez1@hotmail.com>
2022-09-01 13:07:04 -0500

    feat: open test file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::fs::File;          // file libraries
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut file = File::open("ProyectoFinal.txt").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    println!("{}",contents);
 }
