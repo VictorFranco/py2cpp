410526701c92d1574aaa192c2ab4bce4d94e2b94
Victor Franco <vfrancohdez1@hotmail.com>
2022-09-02 07:10:53 -0500

    fix: use relative path

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::fs::File;          // file libraries
 use std::io::prelude::*;
 
 fn main() {
-    let mut file = File::open("ProyectoFinal.txt").unwrap();
+    let mut file = File::open("./src/ProyectoFinal.txt").unwrap();
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
     println!("{}",contents);
