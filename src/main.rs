adc4baf61ad47dcf2b1e37271809704cccacb61f
Victor Franco <vfrancohdez1@hotmail.com>
2022-09-16 14:21:25 -0500

    feat: create vector_2_cpp()
    Read the vector of functions to transpile Python code to C++ code.

--- src/main.rs
+++ src/main.rs
@@ -26,9 +26,9 @@ const DEC_FUN:&str = r"def\s[a-zA-Z][a-zA-Z_-]*\(.*\):(\n\s{4}.*)*";
 // return
 const RETURN:&str = r"return .*";
 
-fn get_return_types(contents: &str) -> Vec<String> {
+fn get_return_types(py_code: &str) -> Vec<String> {
     let re = Regex::new(DEC_FUN).unwrap();
-    let caps = re.captures_iter(contents);
+    let caps = re.captures_iter(py_code);
     let mut returns = Vec::new();
 
     for cap in caps {
@@ -44,12 +44,12 @@ fn get_return_types(contents: &str) -> Vec<String> {
     returns
 }
 
-fn py_2_vector(contents: &str) -> Vec<Function> {
+fn py_2_vector(py_code: &str) -> Vec<Function> {
     let re = Regex::new(HEAD_DEC_FUN).unwrap();
-    let caps = re.captures_iter(contents);
+    let caps = re.captures_iter(py_code);
     let mut functions = Vec::new();
 
-    let return_types: Vec<String> = get_return_types(contents);
+    let return_types: Vec<String> = get_return_types(py_code);
 
     for (index, cap) in caps.enumerate() {
         let mut params = Vec::new();
@@ -75,12 +75,35 @@ fn py_2_vector(contents: &str) -> Vec<Function> {
     functions
 }
 
+fn function_2_cpp(dec_fun: &Function) -> String {
+    let mut result = format!("{} {} (",dec_fun.type_,dec_fun.name);
+    for (index, param) in dec_fun.params.iter().enumerate() {
+        if index > 0 {
+            result.push_str(", ");
+        }
+        result = format!("{}{} {}", result, param.type_, param.name);
+    }
+    result = format!("{}) {{\n}}\n", result);
+    result
+}
+
+fn vector_2_cpp(functions: Vec<Function>) -> String {
+    let mut result = String::new();
+    for function in functions.iter() {
+        result = format!("{}{}\n", result, function_2_cpp(function));
+    }
+    result
+}
+
 fn main() {
     let mut file = File::open("./src/ProyectoFinal.txt").unwrap();
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-    let contents = &contents.to_string();           // store file content
+    let mut py_code = String::new();
+    file.read_to_string(&mut py_code).unwrap();
+    let py_code = &py_code.to_string();           // store file content
 
-    let functions: Vec<Function> = py_2_vector(contents);
+    let functions: Vec<Function> = py_2_vector(py_code);
     println!("{:?}", functions);
+
+    let cpp_code: String = vector_2_cpp(functions);
+    print!("{}", cpp_code);
 }
