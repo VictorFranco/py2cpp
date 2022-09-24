e625c54e85e7ba26cf35bbb857e29be8890384f6
Victor Franco <vfrancohdez1@hotmail.com>
2022-09-24 17:56:56 -0500

    feat: create Code data structure
    Create Code data structure and implement its functionality.
    
    Renamed functions:
    
    function_2_cpp() -> fun2cpp()
    py_2_vector()    -> py2code()
    vector_2_cpp()   -> code2py()

--- src/py2cpp.rs
+++ src/py2cpp.rs
@@ -19,81 +19,105 @@ struct Data {
 struct Function {
     name: String,
     type_: String,
-    params: Vec<Data>,
+    params: Vec<Data>
 }
 
-fn get_return_types(py_code: &str) -> Vec<String> {
-    let re = Regex::new(DEC_FUN).unwrap();
-    let caps = re.captures_iter(py_code);
-    let mut returns = Vec::new();
+#[allow(unused)]
+#[derive(Debug)]
+struct Library {
+    name: String
+}
 
-    for cap in caps {
-        let dec_fun = cap.get(0).unwrap().as_str();
-        let re = Regex::new(RETURN).unwrap();
-        if !re.is_match(dec_fun) {
-            returns.push("void".to_string());
-        }
-        else {
-            returns.push("undefined".to_string());
+#[allow(unused)]
+#[derive(Debug)]
+struct Code {
+    libraries: Vec<Library>,
+    functions: Vec<Function>
+}
+
+impl Code {
+
+    fn create_code() -> Code {
+        Code {
+            libraries: Vec::new(),
+            functions: Vec::new(),
         }
     }
-    returns
-}
 
-fn py2vec(py_code: &str) -> Vec<Function> {
-    let re = Regex::new(HEAD_DEC_FUN).unwrap();
-    let caps = re.captures_iter(py_code);
-    let mut functions = Vec::new();
+    fn get_return_types(py_code: &str) -> Vec<String> {
+        let re = Regex::new(DEC_FUN).unwrap();
+        let caps = re.captures_iter(py_code);
+        let mut returns = Vec::new();
 
-    let return_types: Vec<String> = get_return_types(py_code);
-
-    for (index, cap) in caps.enumerate() {
-        let mut params = Vec::new();
-
-        for i in 2..cap.len() {
-            match cap.get(i) {
-                Some(data) => params.push(
-                    Data {
-                        name: data.as_str().to_string(),
-                        type_: "undefined".to_string(),
-                    }
-                ),
-                None => {}
+        for cap in caps {
+            let dec_fun = cap.get(0).unwrap().as_str();
+            let re = Regex::new(RETURN).unwrap();
+            if !re.is_match(dec_fun) {
+                returns.push("void".to_string());
+            }
+            else {
+                returns.push("undefined".to_string());
             }
         }
-        let fun = Function {
-            name: cap[1].to_string(),
-            type_: return_types[index].to_string(),
-            params
-        };
-        functions.push(fun);
+        returns
     }
-    functions
-}
 
-fn fun2cpp(dec_fun: &Function) -> String {
-    let mut result = format!("{} {} (",dec_fun.type_,dec_fun.name);
-    for (index, param) in dec_fun.params.iter().enumerate() {
-        if index > 0 {
-            result.push_str(", ");
+    fn py2code(py_code: &str) -> Code {
+        let re = Regex::new(HEAD_DEC_FUN).unwrap();
+        let caps = re.captures_iter(py_code);
+        let mut code = Self::create_code();
+
+        let return_types: Vec<String> = Self::get_return_types(py_code);
+
+        for (index, cap) in caps.enumerate() {
+            let mut params = Vec::new();
+
+            for i in 2..cap.len() {
+                match cap.get(i) {
+                    Some(data) => params.push(
+                        Data {
+                            name: data.as_str().to_string(),
+                            type_: "undefined".to_string(),
+                        }
+                    ),
+                    None => {}
+                }
+            }
+            let fun = Function {
+                name: cap[1].to_string(),
+                type_: return_types[index].to_string(),
+                params
+            };
+            code.functions.push(fun);
         }
-        result = format!("{}{} {}", result, param.type_, param.name);
+        code
     }
-    result = format!("{}) {{\n}}\n", result);
-    result
-}
 
-fn vec2cpp(functions: Vec<Function>) -> String {
-    let mut result = String::new();
-    for function in functions.iter() {
-        result = format!("{}{}\n", result, fun2cpp(function));
+    fn fun2cpp(dec_fun: &Function) -> String {
+        let mut result = format!("{} {} (",dec_fun.type_,dec_fun.name);
+        for (index, param) in dec_fun.params.iter().enumerate() {
+            if index > 0 {
+                result.push_str(", ");
+            }
+            result = format!("{}{} {}", result, param.type_, param.name);
+        }
+        result = format!("{}) {{\n}}\n", result);
+        result
     }
-    result
+
+    fn code2cpp(self: Code) -> String {
+        let mut result = String::new();
+        for function in self.functions.iter() {
+            result = format!("{}{}\n", result, Self::fun2cpp(function));
+        }
+        result
+    }
+
 }
 
 pub fn transpile(py_code: &str) -> String {
-    let functions: Vec<Function> = py2vec(py_code);
-    println!("{:?}", functions);
+    let code: Code = Code::py2code(py_code);
+    println!("{:?}", code);
 
-    vec2cpp(functions)
+    Code::code2cpp(code)
 }
