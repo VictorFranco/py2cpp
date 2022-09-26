0666ee00e1cc6e101135720c7c12036cb3d0a722
Victor Franco <vfrancohdez1@hotmail.com>
2022-09-26 18:12:26 -0500

    feat(main.rs): write generated code in main.cpp

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,10 @@ fn main() {
     let mut file = File::open("./src/ProyectoFinal.txt").unwrap();
     let mut py_code = String::new();
     file.read_to_string(&mut py_code).unwrap();
-    let py_code = &py_code.to_string();           // store file content
+    let py_code = &py_code.to_string();                 // store file content
 
     let cpp_code: String = py2cpp::transpile(py_code);
     print!("{}", cpp_code);
+    let mut file = File::create("./src/main.cpp").unwrap();
+    file.write_all(cpp_code.as_bytes()).unwrap();       // write generated code
 }
