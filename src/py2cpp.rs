b704c2dc0cc0db8ef62d41396df290cd8c0b4247
Victor Franco <vfrancohdez1@hotmail.com>
2022-09-27 09:24:50 -0500

    perf: reduce the number of loops
    Create get_header() to get name and parameters of a function.
    
    Renamed functions:
    
    get_bodies()       -> get_body()
    get_return_types() -> get_return_type()

--- src/py2cpp.rs
+++ src/py2cpp.rs
@@ -1,16 +1,16 @@
 use regex::Regex;
 
 // head of declared function
-const HEAD_DEC_FUN:&str = r"def\s([a-zA-Z][a-zA-Z_-]*)\(([a-zA-Z][a-zA-Z0-9]*),?([a-zA-Z][a-zA-Z0-9]*)*\):";
+const HEAD_DEC_FUN: &str = r"def\s([a-zA-Z][a-zA-Z_-]*)\(([a-zA-Z][a-zA-Z0-9]*),?([a-zA-Z][a-zA-Z0-9]*)*\):";
 
 // declared function with body
-const DEC_FUN:&str = r"def\s[a-zA-Z][a-zA-Z_-]*\(.*\):\n((\s{4,}.*\n)*)";
+const DEC_FUN: &str = r"def\s[a-zA-Z][a-zA-Z_-]*\(.*\):\n((\s{4,}.*\n)*)";
 
 // instructions
-const INSTRUCTIONS:&str = r"\s{4,}(.*)\n";
+const INSTRUCTIONS: &str = r"\s{4,}(.*)\n";
 
 // return
-const RETURN:&str = r"return .*";
+const RETURN: &str = r"return .*";
 
 #[derive(Debug)]
 struct Data {
@@ -18,7 +18,7 @@ struct Data {
     type_: String
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 struct Instruction {
     content: String
 }
@@ -49,89 +49,79 @@ impl Code {
     fn create_code() -> Code {
         Code {
             libraries: Vec::new(),
-            functions: Vec::new(),
+            functions: Vec::new()
         }
     }
 
-    fn get_return_types(py_code: &str) -> Vec<String> {
-        let re = Regex::new(DEC_FUN).unwrap();
-        let caps = re.captures_iter(py_code);
-        let mut returns = Vec::new();
+    fn get_header(cap: &regex::Captures) -> (String, Vec<Data>) {
+        let dec_fun = cap.get(0).unwrap().as_str();
+        let re = Regex::new(HEAD_DEC_FUN).unwrap();
+        let cap = re.captures(dec_fun).unwrap();
+        let name = cap.get(1).unwrap().as_str().to_string();
+        let mut params = Vec::new();
 
-        for cap in caps {
-            let dec_fun = cap.get(0).unwrap().as_str();
-            let re = Regex::new(RETURN).unwrap();
-            if !re.is_match(dec_fun) {        // if the function has no return statement
-                returns.push("void".to_string());   // then it is void type
-            }
-            else {
-                returns.push("undefined".to_string());
+        for i in 2..cap.len() {
+            match cap.get(i) {
+                Some(data) => params.push(
+                    Data {
+                        name: data.as_str().to_string(),
+                        type_: "undefined".to_string()
+                    }
+                ),
+                None => {}
             }
         }
-        returns
+        (name, params)
     }
 
-    fn get_bodies(py_code: &str) -> Vec<Vec<Instruction>> {
-        let re = Regex::new(DEC_FUN).unwrap();
-        let caps = re.captures_iter(py_code);
-        let mut returns = Vec::new();
+    fn get_return_type(cap: &regex::Captures) -> String {
+        let dec_fun = cap.get(0).unwrap().as_str();
+        let re = Regex::new(RETURN).unwrap();
+
+        if !re.is_match(dec_fun) {      // if the function has no return statement
+            return "void".to_string();  // then it is void type
+        }
+        else {
+            return "undefined".to_string();
+        }
+    }
+
+    fn get_body(cap: &regex::Captures) -> Vec<Instruction> {
+        let body = cap.get(1).unwrap().as_str();        // extract function body
         let re = Regex::new(INSTRUCTIONS).unwrap();
+        let caps = re.captures_iter(body);
+        let mut instructions: Vec<Instruction> = Vec::new();
 
         for cap in caps {
-            let body = cap.get(1).unwrap().as_str();             // extract function body
-            let mut instructions:Vec<Instruction> = Vec::new();
-            let caps = re.captures_iter(body);
-
-            for cap in caps {
-                let content = cap.get(1).unwrap().as_str().to_string();
-                instructions.push(
-                    Instruction { content }                            // save each instruction
-                );
-            }
-            returns.push(instructions);
+            let content = cap.get(1).unwrap().as_str().to_string();
+            instructions.push(
+                Instruction { content }                 // save each instruction
+            );
         }
-        returns
+        instructions
     }
 
     fn py2code(py_code: &str) -> Code {
-        let re = Regex::new(HEAD_DEC_FUN).unwrap();
-        let caps = re.captures_iter(py_code).enumerate();
+        let re = Regex::new(DEC_FUN).unwrap();
+        let caps = re.captures_iter(py_code);
         let mut code = Self::create_code();
 
-        let types: Vec<String> = Self::get_return_types(py_code);      // get each function type
-        let bodies: Vec<Vec<Instruction>> = Self::get_bodies(py_code); // get each function body
-
-        for (index, cap) in caps {
-            let mut params = Vec::new();
-
-            for i in 2..cap.len() {
-                match cap.get(i) {
-                    Some(data) => params.push(
-                        Data {
-                            name: data.as_str().to_string(),
-                            type_: "undefined".to_string(),
-                        }
-                    ),
-                    None => {}
-                }
-            }
+        for cap in caps {
+            let type_: String = Self::get_return_type(&cap);   // get function type
+            let body: Vec<Instruction> = Self::get_body(&cap); // get function body
+            let (name, params): (String, Vec<Data>) = Self::get_header(&cap);
 
             code.functions.push(
-                Function {
-                    name: cap[1].to_string(),
-                    type_: types[index].to_string(),
-                    params,
-                    body: bodies[index].clone()
-                }
+                Function { name, type_, params, body }
             );
         }
         code
     }
 
-    fn fun2cpp(dec_fun: &Function) -> String {
+    fn fun2cpp(function: &Function) -> String {
         // generate function header
-        let mut result = format!("{} {} (", dec_fun.type_, dec_fun.name);
-        for (index, param) in dec_fun.params.iter().enumerate() {
+        let mut result = format!("{} {}(", function.type_, function.name);
+        for (index, param) in function.params.iter().enumerate() {
             if index > 0 {
                 result.push_str(", ");
             }
@@ -139,7 +129,7 @@ impl Code {
         }
         // generate function body
         let mut body = String::new();
-        for instruction in &dec_fun.body {
+        for instruction in &function.body {
             body = format!("{}{}\n", body, instruction.content);
         }
         result = format!("{}) {{\n{}}}\n", result, body);
