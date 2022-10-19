4107274d83d447c79216b03ae7e4975956e68b82
Victor Franco <vfrancohdez1@hotmail.com>
2022-10-19 08:04:31 -0500

    feat(loop): add range value recognition

--- src/instructions/loop.rs
+++ src/instructions/loop.rs
@@ -1,15 +1,31 @@
 use std::collections::HashMap;
-use crate::py2cpp::{Type, Value, Instruction, Library};
-use crate::constants::RE_LOOP;
+use crate::py2cpp::{Type, Value, Instruction, instruc2value, Library};
+use crate::constants::{RE_LOOP, RE_FUN, RE_INT, RE_VAR};
+use crate::instructions::custom_fun;
 
-pub fn py2code(_body: &mut Vec<Instruction>, _fun_types: &HashMap<String, Type>, content: &str) -> Option<(Vec<Instruction>, Vec<Library>)> {
+pub fn py2code(body: &mut Vec<Instruction>, fun_types: &HashMap<String, Type>, content: &str) -> Option<(Vec<Instruction>, Vec<Library>)> {
     let cap_return = RE_LOOP.captures(content);
 
     match cap_return {
         Some(data) => {
             let counter = data.get(1).unwrap().as_str().to_string();
-            let start = Value::ConstValue(data.get(2).unwrap().as_str().to_string());
-            let end = Value::ConstValue(data.get(3).unwrap().as_str().to_string());
+            let mut values = [Value::None, Value::None];
+            let params = [
+                data.get(2).unwrap().as_str().to_string(),
+                data.get(3).unwrap().as_str().to_string()
+            ];
+            for (index, param) in params.iter().enumerate() {
+                values[index] = match param {
+                    text if RE_INT.is_match(text) => Value::ConstValue(param.to_string()),
+                    text if RE_VAR.is_match(text) => Value::UseVar(param.to_string()),
+                    text if RE_FUN.is_match(text) => {
+                        let (instructions, _libraries) = custom_fun::py2code(body, fun_types, text).unwrap();
+                        instruc2value(&instructions[0])
+                    }
+                    _ => Value::None
+                };
+            }
+            let [start, end] = values;
             let content = vec![];
             let instruction = Instruction::Loop { counter, start, end, content };
             Some((vec![instruction], vec![]))
@@ -22,11 +38,12 @@ pub fn code2cpp(counter: &String, start: &Value, end: &Value, _content: &Vec<Ins
     let params = [start, end];
     let mut values = [String::new(), String::new()];
     for (index, param) in params.iter().enumerate() {
-        match param {
-            Value::ConstValue(data) | Value::UseVar(data) => {
-                values[index] = data.to_string();
-            },
-            _ => {}
+        values[index] = match param {
+            Value::ConstValue(data) | Value::UseVar(data) => data.to_string(),
+            Value::CallFun { name, arguments } => {
+                custom_fun::code2cpp(name, arguments, false)
+            }
+            _ => String::new()
         };
     }
     let [start, end] = values;
